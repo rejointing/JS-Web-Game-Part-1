7458d40f977885af16cec89fb8db5cfe0c5a2ae7
REJOINT <brecab120@gmail.com>
2022-12-11 21:42:58 -0500

    updates

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(){
+    let newCutie= document.createElement('img')
+newCutie.src = 'assets/new-Cutie.gif/png'
+newCutie.style.position = 'fixed'
+newCutie.style.left = '100px'
+newCutie.style.bottom = '100px'
+document.body.append(newCutie)
+}
+newImage(greenCharacter)
+
+}
+//let greenCharacter = document.createElement('img')
+//greenCharacter.src = 'assets/green-character.gif'
+//greenCharacter.style.position = 'fixed'
+//greenCharacter.style.left = '100px'
+//greenCharacter.style.bottom = '100px'
+//document.body.append(greenCharacter)
+
+let pineTree = document.createElement('img')
+pineTree.src = 'assets/pine-tree.png'
+pineTree.style.position = 'fixed'
+pineTree.style.left = '450px'
+pineTree.style.bottom = '200px'
+document.body.append(pineTree)
