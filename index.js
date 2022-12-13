d1653910cf332950ac0166f49beadfd55953b333
REJOINT <brecab120@gmail.com>
2022-12-13 00:19:41 -0500

    uploads

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
-function newImage(){
-    let newCutie= document.createElement('img')
-newCutie.src = 'assets/new-Cutie.gif/png'
-newCutie.style.position = 'fixed'
-newCutie.style.left = '100px'
-newCutie.style.bottom = '100px'
-document.body.append(newCutie)
+function newImage(addyy, l, b){
+    let icon= document.createElement('img')
+icon.src = addyy
+icon.style.position = 'fixed'
+icon.style.left = l +'px'
+icon.style.bottom = b + 'px'
+document.body.append(icon)
+return icon
 }
-newImage(greenCharacter)
+function newItem(addyy, l, b){
+    let icon = newImage(addyy, l, b)
 
+    icon.addEventListener('dblclick', () => {
+        icon.remove()
+    })
 }
+
+newImage('assets/green-Character/south.gif', 100, 100)
+newImage('assets/pine-tree.png',200,300)
+newImage('assets/pillar.png',350,100)
+newImage('assets/crate.png',450,200)
+newImage('assets/well.png',500,425)
+newItem('assets/sword.png',500,405)
+newItem('assets/sheild.png',650,100)
+newItem('assets/staff.png',600,100)
+icon.remove(sword)
+
+//let sword =document.createElement('img')
+//sword.src = 'assets/sword.png'
+//sword.position ='fixed'
+//sword.left='500px'
+//sword.bottom ='405px'
+//document.body.append(sword)
+
+//sword.addEventListener('click', function(){
+    //sword.remove() 
+
+
+
 //let greenCharacter = document.createElement('img')
 //greenCharacter.src = 'assets/green-character.gif'
 //greenCharacter.style.position = 'fixed'
@@ -16,9 +44,9 @@ newImage(greenCharacter)
 //greenCharacter.style.bottom = '100px'
 //document.body.append(greenCharacter)
 
-let pineTree = document.createElement('img')
-pineTree.src = 'assets/pine-tree.png'
-pineTree.style.position = 'fixed'
-pineTree.style.left = '450px'
-pineTree.style.bottom = '200px'
-document.body.append(pineTree)
+//let pineTree = document.createElement('img')
+//pineTree.src = 'assets/pine-tree.png'
+//pineTree.style.position = 'fixed'
+//pineTree.style.left = '450px'
+//pineTree.style.bottom = '200px'
+//document.body.append(pineTree)
